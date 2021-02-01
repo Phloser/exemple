24f6ce82638ff50c375b77253341df6c9a42f624
Phloser <phloser@icloud.com>
2021-02-01 17:35:12 +0300

    Add project files.

---
+++ ConsoleApp4/OutputHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class OutputHelpers
+    {
+        public static void PrintInCenter(string text)
+        {
+            Console.SetCursorPosition((Console.WindowWidth - text.Length) / 2, Console.WindowHeight / 2 - 1);
+            Console.WriteLine(text);
+        }
+
+        public static void Pause()
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp4/summNumb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class summNumb
+    {
+        static void PrintNumber(int a, int b)
+        {
+            if (a <= b)
+            {
+                Console.WriteLine(a);
+                a++;
+                PrintNumber(a, b);
+            }
+        }
+
+        static int SumNumber(int a, int b)
+        {
+            if (a <= b)
+            {
+                var sum = SumNumber(a + 1, b);
+                return a + sum;
+            }
+            return 0;
+        }
+
+        static void Main()
+        {
+            Console.Write("Введите число a: ");
+
+            var aInput = Console.ReadLine();
+            var d = Convert.ToInt32(aInput);
+            Console.Write("Введите число b: ");
+            var bInput = Console.ReadLine();
+            var e = Convert.ToInt32(bInput);
+
+            PrintNumber(d, e);
+            Console.Write($"\nСумма всех чисел равна: { SumNumber(d, e)}");
+
+            Console.ReadKey();
+        }
+    }
+}
